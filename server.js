53b2934f55ec84847d2b528d2d35f05b8adf91e3
Gabriel <gabrielfurniel.gc@outlook.com>
2022-03-13 14:50:38 -0300

    alter server node

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-undef */
 const express = require('express');
-
-const { resolve } = require('path');
-
+const path = require('path');
 const app = express();
 
-app.use('./', express.static(resolve(__dirname, './build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(process.env.PORT || 3000, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log(`Tudo funcionando na porta ${process.env.PORT}`);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.listen(process.env.PORT || 3000);
