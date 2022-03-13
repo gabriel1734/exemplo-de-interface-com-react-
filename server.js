804f7ac300d69420c17d647c3e3ab130fa2d4191
Gabriel <gabrielfurniel.gc@outlook.com>
2022-03-12 22:45:16 -0300

    config express

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('batata');
+/* eslint-disable no-undef */
+const express = require('express');
+
+const { resolve } = require('path');
+
+const app = express();
+
+app.use('./', express.static(resolve(__dirname, './build')));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`Tudo funcionando na porta ${process.env.PORT}`);
+});
